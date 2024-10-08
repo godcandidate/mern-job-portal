10f6d1a85cd67ea9bd4790e8f45be6c3fd905452
godcandidate <godcandidate@gmail.com>
2024-10-08 10:33:01 +0000

    create server files

---
+++ server/app.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+
+
+
+const app = express();
+
+//body parser
+app.use(express.json({limit: "50mb"}));
+
+//cookie parser
+app.use(cookieParser());
+
+//cors
+app.use(cors({
+  origin: process.env.ORIGIN
+}));
+
+
+//testing api
+app.use('/test', (req: Request, res:Response, next:NextFunction) => {
+  res.status(200).json({
+    sucess: true,
+    message: "API is working"
+  });
+}); 
+
+//unkown route
+app.all("*", (req: Request, res:Response, next:NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+//error handler
+//app.use(ErrorMiddleware);
+
+export {app};
+
---
+++ server/server.ts
@@ -0,0 +1,8 @@
+import { app } from "./app";
+import 'dotenv/config';
+
+
+//create server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is connected http://localhost:${process.env.PORT}`);
+});
