301a1e7c73a92b3a1e6af424492e631f78d65b8d
godcandidate <godcandidate@gmail.com>
2024-10-14 18:04:26 +0000

    update route.ts
    Added a path to edit a job

--- server/routes/route.ts
+++ server/routes/route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllJobs, getSingleJob, uploadJob } from "../controllers/job.controller";
+import { editJob, getAllJobs, getSingleJob, uploadJob } from "../controllers/job.controller";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/post-job", uploadJob);
 router.get("/all-jobs", getAllJobs);
 router.get("/jobs/:id", getSingleJob);
+router.put("/jobs/:id", editJob);
 
 export default router;
