e9bc35cedbf855dd1bd98271c91278399026b251
godcandidate <godcandidate@gmail.com>
2024-10-14 17:48:44 +0000

    update route.ts
    Added a route to get a single job

--- server/routes/route.ts
+++ server/routes/route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllJobs, uploadJob } from "../controllers/job.controller";
+import { getAllJobs, getSingleJob, uploadJob } from "../controllers/job.controller";
 
 const router = express.Router();
 
 //Job routes
 router.post("/post-job", uploadJob);
 router.get("/all-jobs", getAllJobs);
+router.get("/jobs/:id", getSingleJob);
 
 export default router;
