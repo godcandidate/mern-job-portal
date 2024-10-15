9baa9d9b3d74c5f767d1f7e1abecbcc72dc1e876
godcandidate <godcandidate@gmail.com>
2024-10-15 12:38:34 +0000

    create auth.ts
    Define authentication for jwt

---
+++ server/utils/auth.ts
@@ -0,0 +1,61 @@
+import { Request, Response, NextFunction } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "./ErrorHandler";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { redis } from "../utils/redis";
+
+import 'dotenv/config';
+
+import { IUser } from "../models/user.model";
+import { resolveSrv } from "dns";
+
+//using interface for req.user
+declare module 'express' {
+  interface Request {
+    user?: IUser;
+  }
+}
+
+//Authenticate user
+export const isAuthenticated = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+          const access_token = req.cookies.access_token;
+
+          if(!access_token){
+              return next(new ErrorHandler("Please login to access this resource", 400));
+          }
+ 
+          //Verify token
+          const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN as string) as JwtPayload;
+          if(!decoded){
+            return next(new ErrorHandler("Access token not valid", 400));
+          }
+
+          //Get user
+          const user = await redis.get(decoded.id) as any;
+          if(!user){
+            return next(new ErrorHandler("User not found", 400));
+          }
+          req.user = JSON.parse(user);
+          next();
+
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+    }
+  );
+
+//validate user role
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user?.role} is not allowed to access this resource`, 403
+        )
+      );
+    }
+    next();
+  };
+};
