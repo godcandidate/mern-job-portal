627f0d460d1b879a0eea840df186e012b2de5ec5
godcandidate <godcandidate@gmail.com>
2024-10-15 13:11:18 +0000

    update route.ts
    Added a route to get user info

--- server/routes/route.ts
+++ server/routes/route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { editJob, getAllJobs, getSingleJob, uploadJob } from "../controllers/job.controller";
-import { activateUser, loginUser, logoutUser, registerUser, updateAccessToken } from "../controllers/user.controller";
+import { activateUser, getUserInfo, loginUser, logoutUser, registerUser, updateAccessToken } from "../controllers/user.controller";
 import { isAuthenticated } from "../utils/auth";
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.post("/activate-user", activateUser);
 router.post("/login-user", loginUser);
 router.post("/logout", isAuthenticated, logoutUser);
 router.post("/refresh", updateAccessToken);
+router.get("/me", isAuthenticated, getUserInfo);
 
 //Job routes
 router.post("/post-job", uploadJob);
