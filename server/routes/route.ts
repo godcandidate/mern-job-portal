7da718f38ce39a62d48d2514737264747623d440
godcandidate <godcandidate@gmail.com>
2024-10-15 08:12:23 +0000

    update route.ts
    Added a path for users to login

--- server/routes/route.ts
+++ server/routes/route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { editJob, getAllJobs, getSingleJob, uploadJob } from "../controllers/job.controller";
-import { activateUser, registerUser } from "../controllers/user.controller";
+import { activateUser, loginUser, registerUser } from "../controllers/user.controller";
 
 const router = express.Router();
 
 //users
 router.post("/registration", registerUser);
 router.post("/activate-user", activateUser);
+router.post("/login-user", loginUser);
 
 //Job routes
 router.post("/post-job", uploadJob);
