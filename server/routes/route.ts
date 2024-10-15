63ed5ee9030129ecf1563a9b00de1b1fab4a8a62
godcandidate <godcandidate@gmail.com>
2024-10-15 12:43:41 +0000

    update route.ts
    Added a route to refresh tokens

--- server/routes/route.ts
+++ server/routes/route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { editJob, getAllJobs, getSingleJob, uploadJob } from "../controllers/job.controller";
-import { activateUser, loginUser, logoutUser, registerUser } from "../controllers/user.controller";
+import { activateUser, loginUser, logoutUser, registerUser, updateAccessToken } from "../controllers/user.controller";
 import { isAuthenticated } from "../utils/auth";
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.post("/registration", registerUser);
 router.post("/activate-user", activateUser);
 router.post("/login-user", loginUser);
 router.post("/logout", isAuthenticated, logoutUser);
+router.post("/refresh", updateAccessToken);
 
 //Job routes
 router.post("/post-job", uploadJob);
